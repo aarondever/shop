c76aa2ddd46ce97f38ce7f3a616d25101c15ba7b
Aaron <troublemaker.5354@gmail>
2023-08-15 22:01:41 +0800

    split settings into developement and production

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+    <footer>
+        <div class="foot_link">
+            <a href="#">关于我们</a>
+            <span>|</span>
+            <a href="#">联系我们</a>
+            <span>|</span>
+            <a href="#">招聘人才</a>
+            <span>|</span>
+            <a href="#">友情链接</a>
+        </div>
+        <p>CopyRight © 2016 北京美多商业股份有限公司 All Rights Reserved</p>
+    </footer>
+</template>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+    <header>
+        <div class="welcome fl">欢迎来到美多商城!</div>
+        <div class="fr">
+            <div class="login_btn fl" v-if="username">
+                欢迎您：<em>[[ username ]]</em>
+                <span>|</span>
+                <a href="/logout/" class="quit">退出</a>
+            </div>
+            <div class="login_btn fl" v-else>
+                <a href="/login/">登录</a>
+                <span>|</span>
+                <a href="/register/">注册</a>
+            </div>
+
+            <div class="user_link fl">
+                <span>|</span>
+                <a href="/info/">用户中心</a>
+                <span>|</span>
+                <a href="/carts/">我的购物车</a>
+                <span>|</span>
+                <a href="/orders/info/1/">我的订单</a>
+            </div>
+        </div>
+    </header>
+</template>
