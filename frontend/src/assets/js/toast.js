66d3317640ad072aac884bafa7a6d4cb615a34a4
Aaron <troublemaker.5354@gmail>
2023-08-26 23:38:49 +0800

    Change favicon.ico

--- frontend/src/assets/js/toast.js
+++
@@ -1,7 +0,0 @@
-import Toast from 'bootstrap/js/dist/toast';
-
-export function showToast(toastId) {
-    const toast = document.getElementById(toastId);
-    const bsToast = Toast.getOrCreateInstance(toast);
-    bsToast.show();
-}
